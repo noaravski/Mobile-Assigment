0e16fc014180a16e7197e3f99aad65e5460712e6
noaravski <noaravski@gmail.com>
2025-01-19 00:22:07 +0200

    remove comments

--- app/src/main/java/com/example/mobile_assigment2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assigment2/MainActivity.kt
@@ -1,21 +1,10 @@
 package com.example.mobile_assigment2
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.example.mobile_assigment2.ui.theme.Mobileassigment2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,10 +16,6 @@ class MainActivity : ComponentActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-//        val addStudentButton: Button = findViewById(R.id.main_activity_add_student_button)
-//        addStudentButton.setOnClickListener {
-//            val intent = Intent(this, AddStudentActivity::class.java)
-//            startActivity(intent)
-//        }
+
     }
 }
