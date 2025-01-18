9dbdd46a049ef1eeaf37a23e38882fa7b80d60fd
noaravski <noaravski@gmail.com>
2025-01-19 00:18:23 +0200

    students list view

--- app/src/main/java/com/example/mobile_assigment2/MainActivity.kt
+++ app/src/main/java/com/example/mobile_assigment2/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.mobile_assigment2
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -11,37 +13,24 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.example.mobile_assigment2.ui.theme.Mobileassigment2Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Mobileassigment2Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Mobileassigment2Theme {
-        Greeting("Android")
+//        val addStudentButton: Button = findViewById(R.id.main_activity_add_student_button)
+//        addStudentButton.setOnClickListener {
+//            val intent = Intent(this, AddStudentActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
