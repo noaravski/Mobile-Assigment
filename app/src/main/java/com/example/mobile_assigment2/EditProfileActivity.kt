55e82f1908db73401ba072712df4a5cb149ee855
idan efraim <idan1000011@walla.com>
2025-01-21 20:36:09 +0200

    updated UI

--- app/src/main/java/com/example/mobile_assigment2/EditProfileActivity.kt
+++ app/src/main/java/com/example/mobile_assigment2/EditProfileActivity.kt
@@ -16,24 +16,25 @@ class EditProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
 
-
         val saveButton = findViewById<Button>(R.id.save_button)
         val cancelButton = findViewById<Button>(R.id.cancel_button)
         val deleteButton = findViewById<Button>(R.id.delete_button)
 
+        val index = intent.getIntExtra("student_index", -1)
+        val nameTextField = findViewById<EditText>(R.id.edit_name)
+        val idTextField = findViewById<EditText>(R.id.edit_id)
+        val isCheckedCheckBox = findViewById<CheckBox>(R.id.edit_is_checked)
+
+        nameTextField.setText(Model.shared.students[index].name)
+        idTextField.setText(Model.shared.students[index].id)
+        isCheckedCheckBox.isChecked = Model.shared.students[index].isChecked
+
         saveButton.setOnClickListener {
-
-            val nameTextField = findViewById<EditText>(R.id.edit_name).text.toString()
-            val idTextField = findViewById<EditText>(R.id.edit_id).text.toString()
-            val isCheckedCheckBox = findViewById<CheckBox>(R.id.edit_is_checked).isChecked
-
-
-            val index = intent.getIntExtra("student_index", -1)
             val updatedStudent = Student(
-                name = nameTextField,
-                id = idTextField,
+                name = nameTextField.text.toString(),
+                id = idTextField.text.toString(),
                 avatarUrl = "",
-                isChecked = isCheckedCheckBox
+                isChecked = isCheckedCheckBox.isChecked
             )
             Log.d("TAG", "before Update student: " + Model.shared.students[index])
             Model.shared.students[index] = updatedStudent
